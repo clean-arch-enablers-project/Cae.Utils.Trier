df779a3a1263ea5b99911f968d5cf84aa3b083e6
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-03-05 18:38:13 -0300

    chore: no retires left mapped exception

---
+++ AutoRetry/NoRetriesLeftException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cae.Utils.MappedExceptions;
+using static Cae.Utils.Trier.AutoRetry.OnExhaustion;
+
+namespace Cae.Utils.Trier.AutoRetry
+{
+    public class NoRetriesLeftException : MappedException
+    {
+        public OnExhaustion.FailureStatus FailureStatus { get; init; }
+
+        public NoRetriesLeftException(string briefPublicMessage, OnExhaustion.FailureStatus failureStatus) : base(
+                    briefPublicMessage,
+                    "All retries (" + failureStatus.TotalOfRetries + ") done for " + failureStatus.Exception
+            )
+        {
+
+            FailureStatus = failureStatus;
+        }
+    }
+}
