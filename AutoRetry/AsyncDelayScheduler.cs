8d2e99b17baca300a295e72f03149fbde0f884cd
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-03-05 18:56:06 -0300

    chore: async delay scheduler

---
+++ AutoRetry/AsyncDelayScheduler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cae.Utils.MappedExceptions.Specifics;
+
+namespace Cae.Utils.Trier.AutoRetry
+{
+    public class AsyncDelayScheduler
+    {
+        public static readonly AsyncDelayScheduler Instance = new();
+
+        private AsyncDelayScheduler() { }
+
+        public static async Task<O> ScheduleWithDelay<O>(Func<Task<O>> supplier, int totalDelayInSeconds)
+        {
+            try
+            {
+                await Task.Delay(totalDelayInSeconds * 1000);
+                return await supplier();
+            }
+            catch (Exception ex)
+            {
+                throw new InternalMappedException(
+                    "An error occurred during the delay or execution.",
+                    "More details: " + ex
+                );
+            }
+        }
+    }
+}
