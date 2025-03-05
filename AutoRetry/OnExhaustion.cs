59abee40cae96a15cbbc3cdcab1938ef2a0721da
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-03-05 18:37:56 -0300

    chore: on exhaustion handler

---
+++ AutoRetry/OnExhaustion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cae.Utils.Trier.AutoRetry
+{
+    public class OnExhaustion
+    {
+        public delegate void HandleFailure(FailureStatus failureStatus);
+
+        public class FailureStatus
+        {
+            public Exception Exception { get; }
+            public int TotalOfRetries { get; }
+
+            public static FailureStatus CreateInstance(Exception exception, int totalOfRetries)
+            {
+                return new FailureStatus(exception, totalOfRetries);
+            }
+
+            private FailureStatus(Exception exception, int totalOfRetries)
+            {
+                Exception = exception;
+                TotalOfRetries = totalOfRetries;
+            }
+        }
+    }
+}
