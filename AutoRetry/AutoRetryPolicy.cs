3a4ac795160d8551e43dbbab0cb5287aba6e9f26
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-03-05 18:38:25 -0300

    feat: basic auto retry policy and tracker

---
+++ AutoRetry/AutoRetryPolicy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cae.Utils.Trier.AutoRetry
+{
+    public class AutoRetryPolicy
+    {
+        public static AutoRetryPolicy CreatePolicy(int maxAmountOfRetries, int baseTimeInSeconds)
+        {
+            return new AutoRetryPolicy(maxAmountOfRetries, baseTimeInSeconds);
+        }
+
+        private AutoRetryPolicy(int maxAmountOfRetries, int baseTimeInSeconds)
+        {
+            BaseTimeInSeconds = baseTimeInSeconds;
+            MaxAmountOfRetries = maxAmountOfRetries;
+            AutoRetryTracker = AutoRetryTracker.CreateTracker(this);
+        }
+
+        public int BaseTimeInSeconds { get; init; }
+        public int MaxAmountOfRetries { get; init; }
+        public AutoRetryTracker AutoRetryTracker { get; init; }
+    }
+}
---
+++ AutoRetry/AutoRetryTracker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cae.Utils.Trier.AutoRetry
+{
+    public class AutoRetryTracker
+    {
+        public static AutoRetryTracker CreateTracker(AutoRetryPolicy autoRetryPolicy)
+        {
+            return new AutoRetryTracker
+            {
+                NumberOfRetriesTriggered = 0,
+                MaxAmountOfRetriesAllowed = autoRetryPolicy.MaxAmountOfRetries,
+                BaseTimeInSeconds = autoRetryPolicy.BaseTimeInSeconds
+            };
+        }
+
+        private int NumberOfRetriesTriggered { get; set; }
+        private int MaxAmountOfRetriesAllowed { get; set; }
+        private int BaseTimeInSeconds { get; set; }
+
+        public int TrackNewAttemptOn(Exception ex)
+        {
+            if (NumberOfRetriesTriggered < MaxAmountOfRetriesAllowed)
+            {
+                int delayToRetry = (int)(BaseTimeInSeconds * Math.Pow(2, NumberOfRetriesTriggered));
+
+                NumberOfRetriesTriggered += 1;
+
+                return delayToRetry;
+            }
+
+            throw new NoRetriesLeftException(
+                "Couldn't proceed with more retries",
+                OnExhaustion.FailureStatus.CreateInstance(ex, NumberOfRetriesTriggered));
+        }
+    }
+}
